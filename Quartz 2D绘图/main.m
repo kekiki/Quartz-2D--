065c444e96a01d9f4c2a148e5e11a48137a25e10
mac <mac@localhost.(none)>
2012-05-14 10:57:42 +0800

    Initial Commit

---
+++ Quartz 2D绘图/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quartz 2D绘图
+//
+//  Created by qwer qwerqw on 12-5-14.
+//  Copyright (c) 2012年 wer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
